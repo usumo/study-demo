43962afb5e07eb739d95f985148ad045ad9c4f2b
halo <wuyfnet@gmail.com>
2021-11-01 14:45:18 +0800

    update

--- vue-demo/src/routes/index.js
+++ vue-demo/src/routes/index.js
@@ -1,42 +1,55 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+// import Vue from 'vue'
+// import Router from 'vue-router'
 
 import BasicLayout from '../layout/BasicLayout';
 
-Vue.use(Router)
+// 解决不需要每个vue文件都需要手动引入，按需加载需手动引入
+// let routes = [];
+// const files = require.context('../views', false, /\.vue/);
+// console.dir(files);
+// console.log(files.keys());
 
-const router = new Router({
-  mode: 'history',
-  scrollBehavior: () => ({ y: 0 }),
-  routes: [
-    {
-      path: '/',
-      component: BasicLayout,
-      children: [
-        {
-          path: '',
-          name: 'index',
-          component: () => import('@/views/index')
-        }
-      ]
-    },
-    {
-      path: '/permission',
-      component: BasicLayout,
-      children: [
-        {
-          path: '',
-          name: 'permission',
-          permission: '10',
-          component: () => import('@/views/permission')
-        }
-      ]
-    },
-    {
-      path: '*',
-      component: () => import('@/views/404')
-    }
-  ]
-})
+// files.keys().forEach(file => {
+//   console.log(files(file).default);
+//   let path = file.split('.')[1];
+//   routes.push({
+//     path,
+//     component: files(file).default
+//   })
+// })
 
-export default router
+// console.log(routes);
+
+// Vue.use(Router)
+
+const routes = [
+  {
+    path: '/',
+    component: BasicLayout,
+    children: [
+      {
+        path: '',
+        name: 'index',
+        component: () => import('@/views/index')
+      }
+    ]
+  },
+  {
+    path: '/permission',
+    component: BasicLayout,
+    children: [
+      {
+        path: '',
+        name: 'permission',
+        permission: '10',
+        component: () => import('@/views/permission')
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: () => import('@/views/404')
+  }
+]
+
+export default routes
